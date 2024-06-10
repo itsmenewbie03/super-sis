d0b84dc2f5caa6548de8036a9bec5e13fb829187
Amiel2003 <2101102216@student.buksu.edu.ph>
2024-06-10 23:28:05 +0800

    - update student feature in student controller

--- routes/students.php
+++ routes/students.php
@@ -7,5 +7,7 @@ Route::middleware('auth')->group(function () {
     Route::prefix('students')->group(function () {
         Route::get('/', [StudentController::class, 'index'])->name('students.index');
         Route::post('/', [StudentController::class, 'post'])->name('students.post');
+        Route::put('/{id}', [StudentController::class, 'update'])->name('students.update');
+
     });
 });
