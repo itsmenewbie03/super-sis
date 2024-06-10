f35ab878f2ad7527e63569409801b7d0b0eec1a5
Amiel2003 <101041775+Amiel2003@users.noreply.github.com>
2024-06-10 22:21:34 +0800

    feat: post function for adding students (#6)

--- routes/students.php
+++ routes/students.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::prefix('students')->group(function () {
         Route::get('/', [StudentController::class, 'index'])->name('students.index');
-        Route::post('/', [StudentController::class, 'index'])->name('students.post');
+        Route::post('/', [StudentController::class, 'post'])->name('students.post');
     });
 });
