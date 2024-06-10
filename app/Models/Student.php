b23c21010bf14a4fd6b7474bed7080dd96513a66
itsmenewbie03 <89230263+itsmenewbie03@users.noreply.github.com>
2024-06-10 19:09:54 +0800

    feat: add more fields to `StudentModel` (#1)
    Co-authored-by: Amiel2003 <2101102216@student.buksu.edu.ph>

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $table = "students";
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'address',
+        'age',
+    ];
+
+    public function grades(){
+        return $this->hasMany(Grade::class);
+    }
 }
