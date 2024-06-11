59024564e9580be70748c6b2e10f23e59fad4cec
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-06-12 01:17:45 +0800

    [FE] -fix landing page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -212,10 +212,13 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="dropdown-item d-flex align-items-center">
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Sign Out</span>
+                    </button>
+                </form>
             </li>
 
           </ul><!-- End Profile Dropdown Items -->
