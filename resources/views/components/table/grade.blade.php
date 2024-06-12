75516ffabcd490787e88b777d7a8c62c287f1a1e
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-12 13:06:58 +0800

    feat: integrate grade module in frontend

--- resources/views/components/table/grade.blade.php
+++ resources/views/components/table/grade.blade.php
@@ -1,7 +1,7 @@
 <div class="card ">
     <div class="card-body">
         <div class="d-flex justify-content-between align-items-center">
-            <h5 class="card-title">Student</h5>
+            <h5 class="card-title">Grades</h5>
         </div>
         <!-- Table with hoverable rows -->
         <div class="table-responsive">
@@ -12,24 +12,41 @@
                         <th scope="col">First Name</th>
                         <th scope="col">Middle Name</th>
                         <th scope="col">Last Name</th>
-                        <th scope="col">Email</th>
+                        <th scope="col">Subject</th>
                         <th scope="col">Grade</th>
                         <th scope="col" class="text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>Smith</td>
-                        <td>John@hmail.com</td>
-                        <td><span class="badge rounded-pill bg-success">1.00</span></td>
-                        <td class="text-center">
-                            <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#editModal">Edit</button>
-                            <button type="button" class="btn btn-sm btn-danger">Delete</button>
-                        </td>
+                    @foreach ($grades as $grade)
+                        <tr>
+                            <td>{{ $grade->id }}</td>
+                            <td>{{ $grade->student->first_name }}</td>
+                            <td>{{ $grade->student->middle_name }}</td>
+                            <td>{{ $grade->student->last_name }}</td>
+                            <td>{{ $grade->subject->subjectname }}</td>
+                            @php
+                                // NOTE: ocd stuff, make badge color based on grade
+                                $bad_grades = ['5.00', 'INC', 'DRP', 'INV(FOR TESTING ONLY!)'];
+                                $warning_grades = ['2.75', '3.00'];
+                                $badge_color = '';
+                                if (in_array($grade->grade, $bad_grades)) {
+                                    $badge_color = 'danger';
+                                } elseif (in_array($grade->grade, $warning_grades)) {
+                                    $badge_color = 'warning';
+                                } else {
+                                    $badge_color = 'success';
+                                }
+                            @endphp
+                            <td><span class="badge rounded-pill bg-{{ $badge_color }}">{{ $grade->grade }}</span></td>
+                            <td class="text-center">
+                                <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#editGradeModal{{ $grade->id }}">Edit</button>
+                                <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#deleteGradeModal{{ $grade->id }}">Delete</button>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
