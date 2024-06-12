789448196253b5b911fb9fe57750accf699065bd
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-12 13:01:30 +0800

    feat: implement CRUD in grade module
    This also implements a validation against invalid grade along with
    ensuring that the subject and student id exists in their respective
    tables.

---
+++ app/Http/Requests/GradeRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GradeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'student_id' => 'required|exists:students,id',
+            'subject_id' => 'required|exists:subjects,id',
+            'grade' => ['required', 'string', function ($attribute, $value, $validator) {
+                $valid_grades = ["1.00", "1.25", "1.50", "1.75", "2.00", "2.25", "2.50", "2.75", "3.00", "5.00","INC","DRP"];
+                if(!in_array($value, $valid_grades)) {
+                    $validator($attribute, 'Invalid grade.');
+                }
+            }]
+        ];
+    }
+}
