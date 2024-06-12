6373b01bf56dff77ddbd9330b66c3d6d8ccf559b
itsmenewbie03 <89230263+itsmenewbie03@users.noreply.github.com>
2024-06-12 13:07:40 +0800

    feat: grades module integration (#14)
    * feat: implement CRUD in grade module
    
    This also implements a validation against invalid grade along with
    ensuring that the subject and student id exists in their respective
    tables.
    
    * feat: add required fields to Grade model
    
    This also implements the FK contraint along with the cascade on
    delete on the migration.
    
    * feat: enable student and subject factory in seeder
    
    * feat: integrate grade module in frontend

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GradeRequest;
 use App\Models\Grade;
+use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class GradeController extends Controller
@@ -14,7 +18,10 @@ class GradeController extends Controller
      */
     public function index(): View|Factory
     {
-        return view("grades.index");
+        $students = Student::all();
+        $subjects = Subject::all();
+        $grades = Grade::all();
+        return view("grades.index", ['students' => $students, 'subjects' => $subjects,'grades' => $grades]);
     }
 
     /**
@@ -28,9 +35,14 @@ class GradeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): void
+    public function store(GradeRequest $request): RedirectResponse
     {
-        //
+        $request->validated();
+        $created = Grade::create($request->all());
+        if($created) {
+            return redirect()->route('grades.index')->with('success', 'Grade added successfully.');
+        }
+        return redirect()->route('grades.index')->with('error', 'Failed to add grade.');
     }
 
     /**
@@ -52,16 +64,25 @@ class GradeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Grade $grade): void
+    public function update(Request $request, Grade $grade): RedirectResponse
     {
-        //
+        $grade->grade = $request->input("grade");
+        $res = $grade->save();
+        if ($res) {
+            return redirect()->route('grades.index')->with('success', 'Grade updated successfully.');
+        }
+        return redirect()->route('grades.index')->with('error', 'Failed to update grade.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Grade $grade): void
+    public function destroy(Grade $grade): RedirectResponse
     {
-        //
+        $isDeleted = $grade->delete();
+        if ($isDeleted) {
+            return redirect()->route('grades.index')->with('success', 'Grade deleted successfully.');
+        }
+        return redirect()->route('grades.index')->with('error', 'Failed to delete grade.');
     }
 }
