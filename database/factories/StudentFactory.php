fa00d579c20d9a2955db58775ac768a85900d66b
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-12 09:07:30 +0800

    feat: allow database seeding
    This will help up test faster by utilizing the factory feature. We now
    have factory for both Student and Subject Models.

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'middle_name' => $this->faker->lastName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'age' => $this->faker->numberBetween(18, 25),
+        ];
+    }
+}
